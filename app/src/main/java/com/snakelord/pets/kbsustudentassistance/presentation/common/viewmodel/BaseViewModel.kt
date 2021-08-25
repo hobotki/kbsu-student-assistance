2fd67a768aa1e50aaadab8d99335f8c9a8a83b18
snakelord757 <blackkiller1527@gmail.com>
2021-08-25 13:02:10 +0300

    Исправил поведение экрана логина при перевороте
    Добавил состояния экрана, которые также будут использованы в будущем

---
+++ app/src/main/java/com/snakelord/pets/kbsustudentassistance/presentation/common/viewmodel/BaseViewModel.kt
@@ -0,0 +1,33 @@
+package com.snakelord.pets.kbsustudentassistance.presentation.common.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.snakelord.pets.kbsustudentassistance.domain.mapper.Mapper
+import com.snakelord.pets.kbsustudentassistance.domain.model.OperationError
+import com.snakelord.pets.kbsustudentassistance.presentation.common.state.UIStates
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
+abstract class BaseViewModel(
+    private val errorMapper: Mapper<Throwable, OperationError>
+) : ViewModel() {
+
+    private val uiStatesMutableLiveData = MutableLiveData<UIStates>()
+    val uiStates: LiveData<UIStates>
+        get() = uiStatesMutableLiveData
+
+    protected val compositeDisposable = CompositeDisposable()
+
+    override fun onCleared() {
+        compositeDisposable.dispose()
+    }
+
+    protected fun updateUIState(uiStates: UIStates) {
+        uiStatesMutableLiveData.value = uiStates
+    }
+
+    protected fun performError(throwable: Throwable) {
+        val error = errorMapper.map(throwable)
+        uiStatesMutableLiveData.value = UIStates.Error(error)
+    }
+}
