89a2fcd89716285f6c1cb0d90578486dbc9f7edb
snakelord757 <blackkiller1527@gmail.com>
2021-09-21 10:24:29 +0300

    Забытая документация

--- app/src/main/java/com/snakelord/pets/kbsustudentassistance/presentation/pass/utils/BitmapUtil.kt
+++ app/src/main/java/com/snakelord/pets/kbsustudentassistance/presentation/pass/utils/BitmapUtil.kt
@@ -8,10 +8,23 @@ import com.google.zxing.common.BitMatrix
 import com.google.zxing.qrcode.QRCodeWriter
 import java.util.EnumMap
 
+/**
+ * Утилитный класс для генерации QR-кода
+ *
+ * @author Murad Luguev on 11-09-2021
+ */
 object BitmapUtil {
     private const val UTF8_CHAR_SET = "UTF-8"
     private const val QR_CODE_MARGINS = 1
 
+    /**
+     * Функция для генерации QR-кода студента
+     *
+     * @param studentData информация о студенте в формате JSON
+     * @param size размер ImageView, в которую будет вложен QR-код
+     *
+     * @return QR-код студента типа [Bitmap]
+     */
     fun generateQrCodeBitmap(studentData: String, size: Int): Bitmap {
         val qrCodeWriter = QRCodeWriter()
         val qcCodeBitMatrix = qrCodeWriter.encode(
